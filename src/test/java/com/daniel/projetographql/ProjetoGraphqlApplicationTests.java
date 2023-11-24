be7851cc3cdf1c21e9d32dfb0c27e271c502038e
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-24 02:51:28 -0300

    Criando projeto SpringBoot3

---
+++ src/test/java/com/daniel/projetographql/ProjetoGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.projetographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
