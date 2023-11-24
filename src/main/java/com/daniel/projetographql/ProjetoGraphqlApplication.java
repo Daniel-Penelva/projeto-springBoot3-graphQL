7ba2277e067b9d57568a1e76aacc4fd460bbf904
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-24 03:44:08 -0300

    Criando um CommandLineRunner para salvar dados da categoria e produto

--- src/main/java/com/daniel/projetographql/ProjetoGraphqlApplication.java
+++ src/main/java/com/daniel/projetographql/ProjetoGraphqlApplication.java
@@ -1,13 +1,53 @@
 package com.daniel.projetographql;
 
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.daniel.projetographql.entities.Categoria;
+import com.daniel.projetographql.entities.Produto;
+import com.daniel.projetographql.repositories.CategoriaRepository;
+import com.daniel.projetographql.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetoGraphqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoGraphqlApplication.class, args);
+
 	}
 
+	// CommandLineRunner executar ações de inicialização ou tarefas específicas quando um aplicativo Spring Boot é inicializado.
+	@Bean
+	CommandLineRunner commandLineRunner(CategoriaRepository categoriaRepository, ProdutoRepository produtoRepository){
+
+		Random random = new Random();
+
+		return args ->{
+			List.of("Computadores", "Impressoras", "Smartphone").forEach(cat -> {
+					Categoria categoria = Categoria.builder().nome(cat).build();
+
+					categoriaRepository.save(categoria);
+				});
+
+				categoriaRepository.findAll().forEach(cat -> {
+					for(int i=0; i<10; i++){
+						Produto produto = Produto.builder()
+							.id(UUID.randomUUID().toString())
+							.nome("Computador" + i)
+							.preco(100 + Math.random() * 50000)
+							.quantidade(random.nextInt(100))
+							.categoria(cat)
+							.build();
+							
+						produtoRepository.save(produto);
+					}
+				});
+			};
+		}
 }
