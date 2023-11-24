f2c9a00c287d63b7ac73dcd6826699f28ed7f589
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-24 03:05:50 -0300

    Criando a camada de modelo Categoria e Produto

---
+++ src/main/java/com/daniel/projetographql/entities/Categoria.java
@@ -0,0 +1,35 @@
+package com.daniel.projetographql.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    //  Uma categoria para muitos produtos - é uma relação bidirecional comm produtos
+    @OneToMany(mappedBy = "categoria")
+    private List<Produto> produtos;
+
+}
